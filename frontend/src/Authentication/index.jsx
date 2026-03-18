ce6c7fc1b36646933c1122efa7eb71a1026b3c45
Thomas van Bommel <thomasvanbommel@hotmail.com>
2026-03-18 13:09:47 -0300

    implemented axios for auth form submission

--- frontend/src/Authentication/index.jsx
+++ frontend/src/Authentication/index.jsx
@@ -1,4 +1,6 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
+import axios from 'axios';
+
 import React, { useState } from 'react'
 
 export default function AuthLayout() {
@@ -9,71 +11,77 @@ export default function AuthLayout() {
     )
 }
 
-export function Login({ setIsAuthenticated }) {
-  return (
-    <form action="/api/login" method="POST">
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" required />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  )
+export function Login() {
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post("/api/login", e.target, { headers: { 'Content-Type': 'application/json' } })
+            .then(res => {
+                if(res.status === 200) {
+                    alert('Login successful!');
+                } else {
+                    alert(`Login failed: ${res.data.message}`);
+                }
+            })
+            .catch(err => {
+                alert(`An error occurred: ${err.message}${'\n' + err.response.data ?? ''}`);
+            });
+    };
+
+    return (
+        <form action="/api/login" onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+        </form>
+    )
 }
 
-export function Register({ setIsAuthenticated }) {
-  const [password, setPassword] = useState('');
-  const [confirm, setConfirm] = useState('');
+export function Register() {
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
 
-  const handleSubmit = e => {
-    e.preventDefault();
+    const navigate = useNavigate();
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post("/api/register", e.target, { headers: { 'Content-Type': 'application/json' } })
+            .then(res => {
+                if(res.status === 201) {
+                    alert('Registration successful! Please log in.');
+                    e.target.reset();
+                    navigate('/login');
+                } else {
+                    alert(`Registration failed: ${res.data.message}`);
+                }
+            })
+            .catch(err => {
+                alert(`An error occurred: ${err.message}${'\n' + err.response.data ?? ''}`);
+            });
+    };
 
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
-    
-    fetch('/api/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }).then(response => {
-      if (response.ok) {
-        setPassword('');
-        setConfirm('');
-        e.target.reset();
-        alert('Registration successful!');
-        setIsAuthenticated(true);
-      } else {
-        response.text().then(text => alert(`Registration failed: ${text}`));
-      }
-    }).catch(error => {
-      alert(`An error occurred: ${error.message}`);
-    });
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" required />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required 
-          onChange={e => setPassword(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="confirm">Confirm :</label>
-        <input type="password" id="confirm" name="confirm" required 
-          onChange={e => setConfirm(e.target.value)} />
-      </div>
-      <button type="submit" disabled={password !== confirm}>Register</button>
-      {password !== confirm && <span style={{ color: 'red' }}> Passwords do not match!</span>}
-    </form>
-  )
+    return (
+        <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required 
+            onChange={e => setPassword(e.target.value)} />
+        </div>
+        <div>
+            <label htmlFor="confirm">Confirm :</label>
+            <input type="password" id="confirm" name="confirm" required 
+            onChange={e => setConfirm(e.target.value)} />
+        </div>
+        <button type="submit" disabled={password !== confirm}>Register</button>
+        {password !== confirm && <span style={{ color: 'red' }}> Passwords do not match!</span>}
+        </form>
+    )
 }
