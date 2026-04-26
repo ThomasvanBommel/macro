38885f10594443c9a427bd5ee7350b557ae5c4a6
Thomas van Bommel <thomasvanbommel@hotmail.com>
2026-04-26 19:17:55 -0300

    more robust error package definition

--- backend/errs/errs.go
+++ backend/errs/errs.go
@@ -1,18 +1,127 @@
 package errs
 
-import "net/http"
+import (
+	"fmt"
+	"log/slog"
+	"maps"
+	"net/http"
+)
 
-type Error struct {
-	code int    // http status code
-	msg  string // public message
-	err  error  // internal error
+type ErrCode int
+
+const (
+	ErrBadCredentials ErrCode = iota
+	ErrNotAuthorized
+	ErrUnexpected
+	ErrBadInput
+	ErrConflict
+	ErrUnknown
+)
+
+var statusMap = map[ErrCode]int{
+	ErrBadCredentials: http.StatusUnauthorized,
+	ErrNotAuthorized:  http.StatusUnauthorized,
+	ErrUnexpected:     http.StatusInternalServerError,
+	ErrBadInput:       http.StatusUnprocessableEntity,
+	ErrConflict:       http.StatusConflict,
+	ErrUnknown:        http.StatusInternalServerError,
 }
 
-func (e *Error) Code() int     { return e.code }
-func (e *Error) Error() string { return e.msg }
-func (e *Error) Unwrap() error { return e.err }
+type Error struct {
+	private  error
+	public   string
+	code     ErrCode
+	metadata map[string]any
+}
 
-func New(code int, msg string, err error) *Error { return &Error{code, msg, err} }
-func Internal(err error) *Error {
-	return New(http.StatusInternalServerError, "An internal error occurred", err)
+func (e *Error) Error() string            { return e.private.Error() }
+func (e *Error) Unwrap() error            { return e.private }
+func (e *Error) Public() string           { return e.public }
+func (e *Error) Code() ErrCode            { return e.code }
+func (e *Error) Metadata() map[string]any { return e.metadata }
+
+// HTTPStatus returns the recommended HTTP status code for this error.
+func (e *Error) HTTPStatus() int {
+	if status, ok := statusMap[e.code]; ok {
+		return status
+	}
+	return http.StatusInternalServerError
+}
+
+// With adds a key-value pair to the error's metadata.
+func (e *Error) With(key string, value any) *Error {
+	if e.metadata == nil {
+		e.metadata = make(map[string]any)
+	}
+	e.metadata[key] = value
+	return e
+}
+
+// New creates a new error with the given message and code.
+//
+// err is the underlying error and is considered private, not exposed to the client.
+//
+// msg is the public message that could be sent to the client.
+//
+// metadata is merged in the order they are provided, overwriting old values. See [maps.Copy]
+//
+//   - If err is nil, a new error is created with the given message and code.
+//   - If err is not nil, it is wrapped with the given message and code.
+//   - If metadata is provided, it is added to the error's metadata.
+//   - If the code is not recognized, ErrUnknown is used.
+func New(err error, msg string, code ErrCode, metadata ...map[string]any) *Error {
+	if _, ok := statusMap[code]; !ok {
+		slog.Error("Unknown error code", "code", code)
+		code = ErrUnknown
+	}
+
+	// Create error if not provided, wrap otherwise
+	if err == nil {
+		err = fmt.Errorf("%s", msg)
+	} else {
+		err = fmt.Errorf("%v: %w", msg, err)
+	}
+
+	// Initialize metadata if provided
+	e := &Error{err, msg, code, nil}
+	if len(metadata) > 0 {
+		e.metadata = metadata[0]
+
+		// Merge additional metadata
+		for _, m := range metadata[1:] {
+			maps.Copy(e.metadata, m)
+		}
+	}
+
+	return e
+}
+
+// BadCredentials is a shortcut for [New] with ErrBadCredentials.
+func BadCredentials(err error, msg string, metadata ...map[string]any) *Error {
+	return New(err, msg, ErrBadCredentials, metadata...)
+}
+
+// NotAuthorized is a shortcut for [New] with ErrNotAuthorized.
+func NotAuthorized(err error, msg string, metadata ...map[string]any) *Error {
+	return New(err, msg, ErrNotAuthorized, metadata...)
+}
+
+// Unexpected is a shortcut for [New] with ErrUnexpected.
+//   - If msg is empty, "Unexpected error" is used.
+func Unexpected(err error, msg string, metadata ...map[string]any) *Error {
+	if msg == "" {
+		msg = "Unexpected error"
+	}
+
+	return New(err, msg, ErrUnexpected, metadata...)
+}
+
+// BadInput is a shortcut for [New] with ErrBadInput.
+func BadInput(err error, msg string, metadata ...map[string]any) *Error {
+	return New(err, msg, ErrBadInput, metadata...)
+}
+
+// Conflict is a shortcut for [New] with ErrConflict.
+func Conflict(err error, msg string, metadata ...map[string]any) *Error {
+	return New(err, msg, ErrConflict, metadata...)
 }
--- backend/errs/errs_test.go
+++ backend/errs/errs_test.go
@@ -2,22 +2,166 @@ package errs
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNew(t *testing.T) {
-	err := New(400, "Bad Request", nil)
-	assert.Equal(t, 400, err.Code())
-	assert.Equal(t, "Bad Request", err.Error())
-	assert.Nil(t, err.Unwrap())
+	t.Parallel()
+
+	// chain creates a chain of errors of the given length, starting with the given error.
+	chain := func(length int, init error) error {
+		t.Helper()
+		e := init
+		for i := range length - 1 {
+			e = fmt.Errorf("%d: %w", i, e)
+		}
+		return e
+	}
+
+	// unwrapChain unwraps the error chain up to the given depth.
+	unwrapChain := func(err error, depth int) error {
+		t.Helper()
+		for range depth + 1 {
+			err = errors.Unwrap(err)
+		}
+		return err
+	}
+
+	defaultChainDepth := 5
+	defaultMetadata := []map[string]any{
+		{"k1": "v1", "k2": "v2"},
+	}
+
+	tests := []struct {
+		name             string
+		err              error
+		public           string
+		code             ErrCode
+		metadata         []map[string]any
+		expectedMetadata map[string]any
+		chainDepth       int
+	}{
+		{
+			name:             "fully-populated",
+			err:              errors.New("Failed command: 'rm -rf --force /'"),
+			public:           "no biggie, try again later",
+			code:             ErrUnknown,
+			metadata:         defaultMetadata,
+			expectedMetadata: defaultMetadata[0],
+			chainDepth:       defaultChainDepth,
+		},
+		{
+			name:             "nil-error",
+			err:              nil,
+			public:           "an ouchie occured :'(",
+			code:             ErrUnknown,
+			metadata:         defaultMetadata,
+			expectedMetadata: defaultMetadata[0],
+			chainDepth:       defaultChainDepth,
+		},
+		{
+			name:             "no-metadata",
+			err:              errors.New("boom! :: boom2!"),
+			public:           "das message 4 da peeps",
+			code:             ErrUnknown,
+			metadata:         nil,
+			expectedMetadata: nil,
+			chainDepth:       defaultChainDepth,
+		},
+		{
+			name:             "invalid-error-code",
+			err:              errors.New("whoopsies!"),
+			public:           "bonjour, welcome to my error",
+			code:             99999,
+			metadata:         defaultMetadata,
+			expectedMetadata: defaultMetadata[0],
+			chainDepth:       defaultChainDepth,
+		},
+		{
+			name:   "metadata-override",
+			err:    errors.New("who put that there?!"),
+			public: "oh well, it's gone now",
+			code:   ErrUnknown,
+			metadata: []map[string]any{
+				defaultMetadata[0],
+				{"k3": "v3", "k1": "v100"},
+			},
+			expectedMetadata: map[string]any{"k1": "v100", "k2": "v2", "k3": "v3"},
+			chainDepth:       defaultChainDepth,
+		},
+		{
+			name:             "very-very-deep",
+			err:              errors.New("look out down below!"),
+			public:           "we're on an adventure, boys",
+			code:             ErrUnknown,
+			metadata:         nil,
+			expectedMetadata: nil,
+			chainDepth:       10000,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			e := chain(tt.chainDepth, New(tt.err, tt.public, tt.code, tt.metadata...))
+
+			var err *Error
+			require.ErrorAs(t, e, &err)
+			assert.Equal(t, tt.public, err.Public())
+			assert.Equal(t, tt.expectedMetadata, err.Metadata())
+
+			// check that the status code is correct
+			status, ok := statusMap[err.Code()]
+			require.True(t, ok, "status code not in statusMap: %v", tt.code)
+			assert.Equal(t, status, err.HTTPStatus())
+
+			// check that the error code is correct
+			if _, ok = statusMap[tt.code]; !ok {
+				assert.Equal(t, ErrUnknown, err.Code())
+			}
+
+			// check that the error is unwrapped correctly
+			assert.Equal(t, tt.err, unwrapChain(e, tt.chainDepth), "not unwrapped correctly")
+		})
+	}
 }
 
-func TestInternal(t *testing.T) {
-	internalErr := errors.New("database connection failed")
-	err := Internal(internalErr)
-	assert.Equal(t, 500, err.Code())
-	assert.Equal(t, "An internal error occurred", err.Error())
-	assert.ErrorIs(t, err.Unwrap(), internalErr)
+func TestWrap(t *testing.T) {
+	t.Parallel()
+
+	// test With builder and overwriting
+	err := New(nil, "test", ErrUnexpected).
+		With("key1", "value1").
+		With("key2", "value2").
+		With("key1", "overwritten")
+
+	expected := map[string]any{
+		"key1": "overwritten",
+		"key2": "value2",
+	}
+
+	assert.Equal(t, expected, err.Metadata())
+}
+
+func TestShortcuts(t *testing.T) {
+	t.Parallel()
+
+	err := BadCredentials(nil, "no, you cannot be Admin...")
+	assert.Equal(t, ErrBadCredentials, err.Code())
+
+	err = NotAuthorized(nil, "this is a holy place, you are not invited!")
+	assert.Equal(t, ErrNotAuthorized, err.Code())
+
+	err = Unexpected(nil, "hmm, didn't see that coming, did yah? (neither did i)")
+	assert.Equal(t, ErrUnexpected, err.Code())
+
+	err = BadInput(nil, "i'm sorry, but your password has to be less than 1GB in size")
+	assert.Equal(t, ErrBadInput, err.Code())
+
+	err = Conflict(nil, "MINE! find your own, pesant")
+	assert.Equal(t, ErrConflict, err.Code())
 }
